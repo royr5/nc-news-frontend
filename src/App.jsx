56917f143687caad41a09958d2d140c756c79f51
roshan <roshanroy.rr@outlook.com>
2023-12-11 13:41:39 +0000

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import ArticleList from "./components/ArticleList/ArticleList";
+import Header from "./components/Header/Header";
+import Nav from "./components/Nav/Nav";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<ArticleList />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/ArticleList/ArticleList.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { getArticles } from "../";
+import ArticleCard from "../ArticleCard/ArticleCard";
+
+export default function ArticleList() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    getArticles().then((res) => {
+      setArticles(res.articles);
+    });
+  }, [articles]);
+
+  return (
+    <>
+      <ul>
+        {articles.map((article) => {
+          return <ArticleCard article={article} key={article.article_id} />;
+        })}
+      </ul>
+    </>
+  );
+}
