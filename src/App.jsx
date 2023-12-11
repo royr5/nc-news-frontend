59679316010dd02890d9708c0befff0078101815
roshan <roshanroy.rr@outlook.com>
2023-12-11 16:41:04 +0000

    add article click

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-
+import ArticleList from "./components/ArticleList/ArticleList";
+import Header from "./components/Header/Header";
+import Nav from "./components/Nav/Nav";
 import Article from "./components/Article/Article";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
+      <Nav />
       <Routes>
+        <Route path="/" element={<ArticleList />} />
         <Route path="/articles/:article" element={<Article />} />
       </Routes>
     </BrowserRouter>
--- src/components/ArticleCard/ArticleCard.jsx
+++ src/components/ArticleCard/ArticleCard.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import "./ArticleCard.css";
+import { Link } from "react-router-dom";
 
 export default function ArticleCard({ article }) {
   const formattedDate = new Date(article.created_at).toLocaleString();
 
   return (
     <li>
-      <h2>{article.title}</h2>
-      <img src={article.article_img_url} alt="" />
-      <p>Topic: {article.topic}</p>
-      <p>By {article.author}</p>
-      <p>
-        Time: <time dateTime={article.created_at}>{formattedDate}</time>
-      </p>
-      <p>Votes: {article.votes}</p>
-
-      <p>Comments: {article.comment_count}</p>
+      <Link to={`/articles/${article.article_id}`} className="article-link">
+        <div>
+          <h2>{article.title}</h2>
+          <img src={article.article_img_url} alt="" />
+          <p>Topic: {article.topic}</p>
+          <p>By {article.author}</p>
+          <p>
+            Time: <time dateTime={article.created_at}>{formattedDate}</time>
+          </p>
+          <p>Votes: {article.votes}</p>
+          <p>Comments: {article.comment_count}</p>
+        </div>
+      </Link>
     </li>
   );
 }
