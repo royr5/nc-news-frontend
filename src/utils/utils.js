7bdee2bde5f30a785746f063386b42c3309ea09f
roshan <roshanroy.rr@outlook.com>
2023-12-13 11:47:24 +0000

    reolve merge conflicts

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -28,11 +28,11 @@ export const getComments = (id) => {
   });
 };
 
-
 export const deleteComment = (id) => {
   return api.delete(`/comments/${id}`).then((res) => {
     console.log(res);
   });
+};
 
 export const postComment = (id, body, username) => {
   return api
@@ -40,5 +40,4 @@ export const postComment = (id, body, username) => {
     .then(({ data }) => {
       return data;
     });
-
 };
