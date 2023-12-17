b9fbf743021a10acbf8e7cddb9d2291bfefdd664
roshan <roshanroy.rr@outlook.com>
2023-12-17 10:54:24 +0000

    style header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,19 @@
-import "./Header.css";
 import { useContext } from "react";
 import { UserContext } from "../../contexts/UserContent";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 export default function Header() {
   const { user } = useContext(UserContext);
 
   return (
-    <>
-      <h1>NC News</h1>
-      <p id="user">{user} is logged in</p>
-    </>
+    <div className="header-container">
+      <Link to="/" className="link">
+        <h1 className="header-title">NC News</h1>
+      </Link>
+      <p id="user">
+        Welcome, <strong>{user}</strong>! You are currently logged in.
+      </p>
+    </div>
   );
 }
