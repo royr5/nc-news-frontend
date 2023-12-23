b74b1404b9f0f13b68e22216ec2df863fc72281e
roshan <roshanroy.rr@outlook.com>
2023-12-23 16:48:09 +0000

    style topics page

--- src/components/Topics/Topics.jsx
+++ src/components/Topics/Topics.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { getTopics } from "../../utils/utils";
 import "./Topics.css";
 import { Link } from "react-router-dom";
+import Spinner from "react-bootstrap/Spinner";
+import { Card, CardGroup } from "react-bootstrap";
 
 export default function Topics() {
   const [topics, setTopics] = useState([]);
@@ -14,23 +16,26 @@ export default function Topics() {
     });
   }, []);
 
-  if (isLoading) {
-    return <h2>Loading...</h2>;
-  }
-
   return (
     <div className="topic-container">
       <h2>All Topics</h2>
-      <ul>
-        {topics.map((topic, index) => (
-          <li className="topic" key={index}>
-            <Link to={`/topics/${topic.slug}`} className="topic-link">
-              <h3>{topic.slug}</h3>
-              <p>{topic.description}</p>
-            </Link>
-          </li>
-        ))}
-      </ul>
+
+      {isLoading ? (
+        <Spinner animation="border" variant="primary" className="spinner" />
+      ) : null}
+
+      <CardGroup id="topics-list">
+        <ul>
+          {topics.map((topic, index) => (
+            <Card className="topic" key={index}>
+              <Link to={`/topics/${topic.slug}`} className="topic-link">
+                <Card.Title className="topic-title">{topic.slug}</Card.Title>
+                <Card.Text >{topic.description}</Card.Text>
+              </Link>
+            </Card>
+          ))}
+        </ul>
+      </CardGroup>
     </div>
   );
 }
